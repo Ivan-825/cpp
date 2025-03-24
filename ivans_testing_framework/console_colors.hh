f4e48bf57a13ddc516cecb9afc7f4668377b6854
Ivan <ivanlaszlo25@gmail.com>
2025-03-24 02:22:55 +0100

    Added differentation for Linux and MacOS.

--- ivans_testing_framework/console_colors.hh
+++ ivans_testing_framework/console_colors.hh
@@ -1,16 +1,26 @@
 #ifndef COLOR_CODES_H
 #define COLOR_CODES_H
 
-#ifdef _WIN32 // Check if the OS is Windows
+#ifdef _WIN32
     static inline const char* GREEN = "\033[32m";
     static inline const char* RED = "\033[31m";
     static inline const char* CYAN = "\033[36m";
     static inline const char* DEF = "\033[0m";
-#else // For non-Windows OS (Linux, macOS, etc.)
+#elif defined(__linux__) || defined(linux) || defined(__gnu_linux__)
+    static inline const char* GREEN = "";
+    static inline const char* RED = "";
+    static inline const char* CYAN = "";
+    static inline const char* DEF = "";
+#elif defined(__APPLE__) && defined(__MACH__)
+    static inline const char* GREEN = "";
+    static inline const char* RED = "";
+    static inline const char* CYAN = "";
+    static inline const char* DEF = "";
+#else
     static inline const char* GREEN = "";
     static inline const char* RED = "";
     static inline const char* CYAN = "";
     static inline const char* DEF = "";
 #endif
 
-#endif // COLOR_CODES_H
+#endif
