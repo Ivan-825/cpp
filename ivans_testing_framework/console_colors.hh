d2e056ad73bec3fe75414d3e6c22471486c27ffc
Ivan <ivanlaszlo25@gmail.com>
2025-03-29 01:13:48 +0100

    Removed unreasonable OS differentation, fixed up the color definitons.

--- ivans_testing_framework/console_colors.hh
+++ ivans_testing_framework/console_colors.hh
@@ -2,25 +2,25 @@
 #define COLOR_CODES_H
 
 #ifdef _WIN32
-    static inline const char* GREEN = "\033[32m";
-    static inline const char* RED = "\033[31m";
-    static inline const char* CYAN = "\033[36m";
-    static inline const char* DEF = "\033[0m";
+    const char* GREEN = "\033[32m";
+    const char* RED = "\033[31m";
+    const char* CYAN = "\033[36m";
+    const char* DEF = "\033[0m";
 #elif defined(__linux__) || defined(linux) || defined(__gnu_linux__)
-    static inline const char* GREEN = "";
-    static inline const char* RED = "";
-    static inline const char* CYAN = "";
-    static inline const char* DEF = "";
+    const char* GREEN = "\033[32m";
+    const char* RED = "\033[31m";
+    const char* CYAN = "\033[36m";
+    const char* DEF = "\033[0m";
 #elif defined(__APPLE__) && defined(__MACH__)
-    static inline const char* GREEN = "";
-    static inline const char* RED = "";
-    static inline const char* CYAN = "";
-    static inline const char* DEF = "";
+    const char* GREEN = "";
+    const char* RED = "";
+    const char* CYAN = "";
+    const char* DEF = "";
 #else
-    static inline const char* GREEN = "";
-    static inline const char* RED = "";
-    static inline const char* CYAN = "";
-    static inline const char* DEF = "";
+    const char* GREEN = "";
+    const char* RED = "";
+    const char* CYAN = "";
+    const char* DEF = "";
 #endif
 
 #endif
